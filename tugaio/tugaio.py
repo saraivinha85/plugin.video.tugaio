3f75a89c4400e6bba1d509ee01c95135e93203af
saraivinha85 <pedro.a34195@gmail.com>
2015-09-05 20:58:12 +0100

    Anti Cloudflare DDOS protection

--- tugaio/tugaio.py
+++ tugaio/tugaio.py
@@ -7,7 +7,7 @@ import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'lib'))
 
-import httplib
+from net import Net
 from bs4 import BeautifulSoup
 import re
 import urllib2
@@ -16,6 +16,7 @@ import urlparse
 import xbmc
 import xbmcgui
 import xbmcplugin
+import cookielib
 import xbmcaddon
 
 TUGA_IO_URL = 'http://tuga.io'
@@ -25,13 +26,172 @@ TUGA_IO_SERIES = "/series/{page}?orderby={order}&from={latest}&genre={genre}"
 TUGA_IO_PAGE_SIZE = 42
 
 
-def create_request(url):
-    httplib.HTTPConnection.debuglevel = 1
+def cf_evaluate_js_string(string):
+    try:
+        offset = 1 if string[0] == '+' else 0
+        return int(
+            eval(string.replace('!+[]', '1').replace('!![]', '1').replace('[]', '0').replace('(', 'str(')[offset:]))
+    except:
+        return
+
+
+def cf_decrypt_ddos(url, agent, cookie_file):
+    class NoRedirection(urllib2.HTTPErrorProcessor):
+        def http_response(self, request, response):
+            return response
+
+    if len(agent) == 0:
+        agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0'
+
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(NoRedirection, urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = [('User-Agent', agent)]
+    response = opener.open(url)
+
+    try:
+        set_cookie = str(response.headers.get('Set-Cookie'))
+    except:
+        set_cookie = ''
+
+    print ['Set-Cookie', set_cookie]
+
+    responce_html = response.read()
+
+    js_header = '<link rel="shortcut icon" href="/Content/images/favicon.ico" type="image/x-icon" /><script type="text/javascript">'
+    if js_header in responce_html:
+        responce_html = responce_html.split(js_header)[-1]
+
+    jschl = re.compile('name="jschl_vc" value="(.+?)"/>').findall(responce_html)[0]
+    print ['jschl', jschl]
+
+    try:
+        cf_pass = re.compile('name="pass" value="(.+?)"/>').findall(responce_html)[0]
+    except:
+        cf_pass = ''
+
+    print ['cf_pass', cf_pass]
+
+    try:
+        cf_challenge_form = \
+            re.compile('<form id="challenge-form" action="(/[^"]+)" method="\D+">').findall(responce_html)[0]
+    except:
+        cf_challenge_form = '/cdn-cgi/l/chk_jschl'
+
+    print ['cf_challenge_form', cf_challenge_form]
+
+    init = re.compile(
+        'setTimeout\(function\(\){\s*\n*\s*(?:var \D,\D,\D,\D, [0-9A-Za-z]+={"[0-9A-Za-z]+"|.*?.*):(.*?)};').findall(
+        responce_html)[-1]
+    print ['init', init]
+
+    builder = re.compile(r"challenge-form\'\);\s*\n*\r*\a*\s*(.*)a.v").findall(responce_html)[0]
+    print ['builder', builder]
+
+    try:
+        wait_time = int(re.compile(r"f.submit\(\);\s*\n*\s*},\s*(\d+)\)").findall(responce_html)[-1])
+    except:
+        wait_time = 5000
+
+    print ['wait_time', wait_time]
+
+    decrypt_value = cf_evaluate_js_string(init)
+    print ['value_to_decrypt', decrypt_value]
+
+    lines = builder.split(';')
+    for line in lines:
+        if len(line) > 0 and '=' in line:
+            try:
+                sections = line.split('=')
+                line_val = cf_evaluate_js_string(sections[1])
+                decrypt_value = int(eval(str(decrypt_value) + sections[0][-1] + str(line_val)))
+            except:
+                pass
+
+    print ['decrypted_value', decrypt_value]
+
+    hostname = get_domain_from_url(url)
+    base_url = "http://" + hostname
+    print ['base_url', base_url]
+    print ['hostname', hostname, len(hostname)]
+
+    answer = decrypt_value + len(hostname)
+    print ['answer', answer]
+
+    if cf_pass == '':
+        query = '%s%s?jschl_vc=%s&jschl_answer=%s' % (str(base_url), str(cf_challenge_form), str(jschl), str(answer))
+    else:
+        query = '%s%s?jschl_vc=%s&pass=%s&jschl_answer=%s' % (
+            str(base_url), str(cf_challenge_form), str(jschl), str(cf_pass), str(answer))
+    print ['query', query]
+
+    xbmc.sleep(wait_time)
+
+    opener = urllib2.build_opener(NoRedirection, urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = [('User-Agent', agent)]
+    response = opener.open(query)
+    cookie = str(response.headers.get('Set-Cookie'))
+    print ['Set-Cookie', cookie]
+
+    cj = cookielib.LWPCookieJar(cookie_file)
+    cj.clear()
     request = urllib2.Request(url)
-    request.add_header('User-Agent',
-                       'Mozilla/5.0 (compatible;)')
-    opener = urllib2.build_opener()
-    return opener.open(request).read()
+    cj.extract_cookies(response, request)
+    response.close()
+
+    final_response = opener.open(request)
+    if final_response.code == 200:
+        cj.save(cookie_file)
+        return True
+
+    return False
+
+
+def get_domain_from_url(url):
+    hostname = urlparse.urlparse(url).hostname
+    if '/' in hostname:
+        hostname = hostname.replace('/', '')
+
+    if 'www.' in hostname:
+        hostname = hostname.replace('www.', '')
+
+    return hostname
+
+
+def is_cookie_expired(url, net):
+    domain = "." + get_domain_from_url(url)
+    print domain
+    for cookie in net._cj:
+        if cookie.domain == domain:
+            return cookie.is_expired()
+
+    return True
+
+
+def cf_generate_new_cookie(url, user_agent, cookie_file):
+    retries = 10
+    while retries > 0 and not cf_decrypt_ddos(url, user_agent, cookie_file):
+        xbmc.sleep(2000)
+        retries -= 1
+
+    return retries > 0
+
+
+def create_request(url, data=None):
+    try:
+        net._cj.clear()
+        return net.http_GET(url).content
+
+    except:
+        # Possible Cloudflare DDOS Protection
+        net._cj.clear()
+        #net.set_cookies(cf_cookie_file)
+
+        #if is_cookie_expired(url, net):
+        print "cookie_expired"
+        cf_generate_new_cookie(url, user_agent, cf_cookie_file)
+        net.set_cookies(cf_cookie_file)
+
+        return net.http_GET(url).content
 
 
 def create_titles(raw_titles):
@@ -65,14 +225,12 @@ def create_title(raw_title):
 def get_movie_titles(page=1, order=2, latest=1, genre=0):
     html = create_request(TUGA_IO_URL +
                           TUGA_IO_MOVIES.format(page=page, order=order, latest=latest, genre=genre))
-
     return find_titles(html, 'filme')
 
 
 def get_tv_titles(page=1, order=2, latest=1, genre=0):
     html = create_request(TUGA_IO_URL +
                           TUGA_IO_SERIES.format(page=page, order=order, latest=latest, genre=genre))
-
     return find_titles(html, 'serie')
 
 
@@ -89,7 +247,6 @@ def get_tv_season_titles(url, season):
 def get_kids_titles(page=1, order=2, latest=1, genre=0):
     html = create_request(TUGA_KIDS_URL +
                           TUGA_IO_MOVIES.format(page=page, order=order, latest=latest, genre=genre))
-
     return find_titles(html, 'filme')
 
 
@@ -106,6 +263,13 @@ def find_seasons(html):
 def find_tv_titles(html, season):
     query = BeautifulSoup(html, "html.parser")
     return query.find('h2', text=season).parent(href=re.compile("/episodio/"))
+
+
+def search():
+    search_data = urllib.urlencode({"procurar": "archer"})
+    html = create_request(TUGA_IO_URL + "/procurar")
+    movies = find_titles(html, 'filme')
+    # series = find_titles(html, 'serie')
 
 
 def build_url(query):
@@ -164,8 +328,8 @@ def create_titles_menu():
         title['base_url'] = tugaio_base_url
 
         url = build_url(title)
-        title_item = xbmcgui.ListItem(title['name'], iconImage=tugaio_base_url + title['thumb'],
-                                      thumbnailImage=tugaio_base_url + title['thumb'])
+        title_item = xbmcgui.ListItem(title['name'], iconImage=build_url_with_cookie(tugaio_base_url, title["thumb"]),
+                                      thumbnailImage=build_url_with_cookie(tugaio_base_url, title["thumb"]))
         title_item.setInfo('Video', {'Year': title['year'], 'Rating': title['imdb'], })
         title_item.setProperty('IsPlayable', is_playable)
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=title_item, isFolder=is_folder)
@@ -178,6 +342,16 @@ def create_titles_menu():
 
     xbmc.executebuiltin("Container.SetViewMode(%s)" % addon.getSetting(view))
     xbmcplugin.endOfDirectory(addon_handle)
+
+
+def build_url_with_cookie(base_url, relative_path=''):
+    tuga_io_cookie_key = '.tuga.io'
+
+    if tuga_io_cookie_key in net._cj._cookies:
+        return base_url + relative_path + '|User-Agent=' + user_agent + '&Cookie=cf_clearance=' +\
+           net._cj._cookies[tuga_io_cookie_key]['/']["cf_clearance"].value
+
+    return base_url + relative_path
 
 
 def create_seasons_menu():
@@ -206,8 +380,8 @@ def create_episodes_menu():
         title['base_url'] = TUGA_IO_URL
 
         url = build_url(title)
-        title_item = xbmcgui.ListItem(title['name'], iconImage=TUGA_IO_URL + title['thumb'],
-                                      thumbnailImage=TUGA_IO_URL + title['thumb'])
+        title_item = xbmcgui.ListItem(title['name'], iconImage=build_url_with_cookie(TUGA_IO_URL, title["thumb"]),
+                                      thumbnailImage=build_url_with_cookie(TUGA_IO_URL, title["thumb"]))
         title_item.setProperty('IsPlayable', "true")
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=title_item)
 
@@ -222,9 +396,9 @@ def play_title():
     tugaio_base_url = args.get('base_url', None)[0]
 
     resolved_url = resolve_video_and_subtitles_url(tugaio_base_url, url)
-    play_item = xbmcgui.ListItem(name, iconImage=tugaio_base_url + thumb, thumbnailImage=tugaio_base_url + thumb,
+    play_item = xbmcgui.ListItem(name, iconImage=build_url_with_cookie(tugaio_base_url, thumb), thumbnailImage=build_url_with_cookie(tugaio_base_url, thumb),
                                  path=resolved_url["video"])
-    play_item.setSubtitles([resolved_url["subtitles"]])
+    play_item.setSubtitles([build_url_with_cookie(resolved_url["subtitles"])])
     xbmcplugin.setResolvedUrl(handle=addon_handle, succeeded=True, listitem=play_item)
 
 
@@ -246,6 +420,12 @@ base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 args = urlparse.parse_qs(sys.argv[2][1:])
 xbmcplugin.setContent(addon_handle, 'movies')
+
+net = Net()
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0'
+cf_cookie_file = os.path.join(os.path.dirname(__file__), 'tugaio.cache')
+net.set_user_agent(user_agent)
+
 
 if len(args) == 0:
     create_root_menu()
